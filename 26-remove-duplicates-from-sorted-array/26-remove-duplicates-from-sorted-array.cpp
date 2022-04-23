441355a5500776ab5f034fe2d7d2d1a8c342a68c
Lucifer._.,Utkarsh <61401484+Utkarshking@users.noreply.github.com>
2022-04-23 20:15:57 +0530

    Time: 12 ms (77.76%), Space: 19.2 MB (5.75%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size()==0) return 0;
-        int left=0;
-        int right=1;
-        for(right=1;right<nums.size();right++){
-            if(nums[left]!=nums[right]) left++;
-            nums[left]=nums[right];
+        // if(nums.size()==0) return 0;
+        // int left=0;
+        // int right=1;
+        // for(right=1;right<nums.size();right++){
+        //     if(nums[left]!=nums[right]) left++;
+        //     nums[left]=nums[right];
+        // }
+        // return left+1;
+        // again a approach using an unordered_map can be done
+        unordered_map<int,int>mpp;
+        // took an extra vector to take the elements out from it 
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            if(mpp[nums[i]]==0)
+                ans.push_back(nums[i]);
+            mpp[nums[i]]++;
         }
-        return left+1;
+        nums=ans;
+        return ans.size();
     }
 };
