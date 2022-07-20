ea1b7e224f3b9e290d228f48eddb1e89970057cd
Lucifer:)Utkarsh <61401484+Utkarshking@users.noreply.github.com>
2022-07-20 22:54:51 +0530

    Time: 20 ms (40.72%), Space: 12.1 MB (21.89%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -11,23 +11,41 @@
  */
 class Solution {
 public:
-    int returnsumheight(TreeNode* root,int &cnt){
-        if(!root) return 0;
-        cnt++;
-        int left=returnsumheight(root->left,cnt);
-        int right=returnsumheight(root->right,cnt);
-        return(root->val+left+right);
-    }
-    void helper(TreeNode* root,int &ans){
-        if(!root) return;
-        int cnt=0;
-        int avg=0;
-        avg=(returnsumheight(root,cnt)/cnt);
-        if(avg==root->val) ans++;
-        helper(root->left,ans);
-        helper(root->right,ans);
+    // time complexity -->O(N^2)
+    // space complexity-->O(1)
+//     int returnsumheight(TreeNode* root,int &cnt){
+//         if(!root) return 0;
+//         cnt++;
+//         int left=returnsumheight(root->left,cnt);
+//         int right=returnsumheight(root->right,cnt);
+//         return(root->val+left+right);
+//     }
+//     void helper(TreeNode* root,int &ans){
+//         if(!root) return;
+//         int cnt=0;
+//         int avg=0;
+//         avg=(returnsumheight(root,cnt)/cnt);
+//         if(avg==root->val) ans++;
+//         helper(root->left,ans);
+//         helper(root->right,ans);
       
-        }
+//         }
+    // optimize-1
+    // we can have pairs and can store the sum of the left subtree and rightsubtree as well
+   pair<int,int> helper(TreeNode* root, int &ans){
+        if(!root) return {0,0};
+       // pair will calculate the sum and count of nodes 
+       // ie.{sum_of_subtree,count_of_nodes_subtree}
+        pair<int,int> left=helper(root->left,ans);
+        pair<int,int> right=helper(root->right,ans);
+       // counting the nodes and calculating the sum for the same
+        int sum=left.first+right.first+root->val;
+        int cnt=left.second+right.second+1;
+        if(sum/cnt==root->val) ans++;
+        return{sum,cnt};
+       
+       
+    }
     int averageOfSubtree(TreeNode* root) {
         int ans=0;
         helper(root,ans);
