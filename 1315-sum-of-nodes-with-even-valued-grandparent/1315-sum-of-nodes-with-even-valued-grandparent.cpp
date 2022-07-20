67cd2647df6e748fc2a3a64577ec14fd1efa7766
Lucifer:)Utkarsh <61401484+Utkarshking@users.noreply.github.com>
2022-07-21 00:46:11 +0530

    Time: 123 ms (5.01%), Space: 47.4 MB (5.23%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -14,12 +14,15 @@ public:
     int sumEvenGrandparent(TreeNode* root) {
         if(!root) return 0;
         int ans=0;
+        // we will create a hashmap that will store all our grand_parent_pointers to their respective grandchilds
         unordered_map<TreeNode*,TreeNode*>grand_parent_pointer;
         queue<TreeNode*>q;
         q.push(root);
         while(!q.empty()){
             auto node=q.front();
             q.pop();
+            // this will ensure if the left->left and left->right is present if yes we fill store its both childs 
+            // if possible
             if(node->left){
                 q.push(node->left);
                 if(node->left->left){
@@ -29,6 +32,7 @@ public:
                     grand_parent_pointer[node->left->right]=node;
                 }
             }
+            // this will ensure if the right->left and right->right is present which will have both the grandchild nodes
             if(node->right){
              q.push(node->right);
                 if(node->right->right){
