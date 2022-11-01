c0199d3d9c8ce3afe4861186a08d4305db27dbe7
Lucifer:)Utkarsh <61401484+Utkarshking@users.noreply.github.com>
2022-11-01 12:38:13 +0530

    Time: 29 ms (28.89%), Space: 8.1 MB (57.83%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,13 +9,30 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* dummy =new ListNode();
-        dummy=head;
-        unordered_map<ListNode*,int>mpp;
-        while(dummy!=NULL){
-            if(mpp.find(dummy)!=mpp.end()) return true;
-            mpp[dummy]=dummy->val;
-            dummy=dummy->next;
+        // time complexity -->O(N)
+        // space complexity -->O(1)
+        // ListNode* dummy =new ListNode();
+        // dummy=head;
+        // unordered_map<ListNode*,int>mpp;
+        // while(dummy!=NULL){
+        //     if(mpp.find(dummy)!=mpp.end()) return true;
+        //     mpp[dummy]=dummy->val;
+        //     dummy=dummy->next;
+        // }
+        // return false;
+        
+        // time complexity -->O(N)
+        // space complexity -->O(1)
+        // having the single or no node case
+        if(head==NULL or head->next==NULL) return false;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next!=NULL and fast->next->next!=NULL){
+            // slow moving at 1x
+            slow=slow->next;
+            // fast moving at 2x
+            fast=fast->next->next;
+            if(slow==fast) return true;
         }
         return false;
     }
