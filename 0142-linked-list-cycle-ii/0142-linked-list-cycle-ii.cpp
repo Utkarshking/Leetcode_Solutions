e41a1ccb28db01e8650d315fc4385a8833c6482c
Lucifer:)Utkarsh <61401484+Utkarshking@users.noreply.github.com>
2022-11-02 17:30:36 +0530

    Time: 18 ms (55.26%), Space: 7.6 MB (75.43%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,12 +9,39 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_map<ListNode*,int>mpp;
-        ListNode* dummy=head;
-        while(dummy!=NULL){
-            if(mpp.find(dummy)!=mpp.end()) return dummy;
-            mpp[dummy]=dummy->val;
-            dummy=dummy->next;
+        // time complexity -->O(N)
+        // space complexity -->O(N)
+        // space used in unordered map(which is the hash table)
+        // unordered_map<ListNode*,int>mpp;
+        // ListNode* dummy=head;
+        // while(dummy!=NULL){
+        //     if(mpp.find(dummy)!=mpp.end()) return dummy;
+        //     mpp[dummy]=dummy->val;
+        //     dummy=dummy->next;
+        // }
+        // return NULL;
+        
+        
+        // time complexity -->~O(N)
+        // space complexity -->O(1)
+        if(head==NULL or head->next==NULL) return NULL;
+        ListNode* fast=head;
+        ListNode* slow=head;
+        ListNode* entry=head;
+        while(fast->next!=NULL and fast->next->next!=NULL){
+                slow=slow->next;
+                fast=fast->next->next;
+                // now in this case 
+                // l1=l2- noofturns*cyclelength
+                // so finding the starting point of the loop in the linked list
+                if(fast==slow){
+                while(entry!=slow){
+                    entry=entry->next;
+                    slow=slow->next;
+                }
+                // now entry has reached the starting the point of the loop in the linked list
+            return entry;
+            }
         }
         return NULL;
     }
